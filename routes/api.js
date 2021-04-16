0215da43c8707a6343f5e0426fef09b906198476
Sofyan AMV <77225794+SofyanAMV09@users.noreply.github.com>
2021-04-16 17:01:28 +0700

    api.js

--- routes/api.js
+++ routes/api.js
@@ -40,7 +40,7 @@ loghandler = {
         creator: `${creator}`,
         code: 406,
         message: 'masukan parameter apikey',
-        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/6282363352693 , key nya gratis kok gan, jadi santuyy ajaa'
+        getApikey: 'gak punya apikey? chat gw aja yaaa di wa.me/6281227825649 , key nya gratis kok gan, jadi santuyy ajaa'
     },
     notkey: {
         status: false,
